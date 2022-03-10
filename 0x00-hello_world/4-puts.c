f3045e573f1b5747f3f4f603a18a89006c88b31e
ashu hg <ashenafihagos.ah@gmail.com>
2022-03-11 00:45:15 +0300

    sa

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+Lightpillar, [3/11/2022 12:41 AM]
+#include <stdio.h>
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,6 @@
-
-/**
- * main - Entry point
- *Return: Always 0 (Success)
- */
+#include <stdio.h>
 int main(void)
 {
- printf("with proper grammar, but the outcome is a piece of art,\n");
- return (0);
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
 }
