f4561604b8b7ced1d5e48ef62571c53f1d1ba7be
ashu hg <ashenafihagos.ah@gmail.com>
2022-03-15 21:58:23 +0300

    s

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 /**
-* main - check the code.
-*
-* Return: Always 0.
-*/
-int main(void)
+ * print_times_table - prints the times table
+ * @n: takes in an integer
+ */
+void print_times_table(int n)
 {
-char x[8] = "_putchar";
-int i = 0;
+ int i, j, k;
 
-while (i <= 7)
-{
-_putchar(x[i]);
-i++;
-}
-_putchar('\n');
-return (0);
+ if (n > 15 || n < 0)
+  return;
+ for (i = 0; i <= n; i++)
+ {
+  for (j = 0; j <= n; j++)
+  {
+   k = i * j;
+   if (j == 0)
+   {
+    _putchar(k + '0');
+   }
+   else if (k <= 9)
+   {
+    _putchar(',');
+    _putchar(' ');
+    _putchar(' ');
+    _putchar(' ');
+    _putchar(k + '0');
+   }
+   else if (k >= 10 && k < 100)
+   {
+    _putchar(',');
+    _putchar(' ');
+    _putchar(' ');
+    _putchar(k / 10 + '0');
+    _putchar(k % 10 + '0');
+   }
+   else
+   {
+    _putchar(',');
+    _putchar(' ');
+    _putchar((k / 100) + '0');
+    _putchar(k / 10 % 10 + '0');
+    _putchar(k % 10 + '0');
+   }
+  }
+  _putchar('\n');
+ }
 }
