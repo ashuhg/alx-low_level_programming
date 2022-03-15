758fc1c0a66877d7d8a2751c8bb68f281a574acc
ashu hg <ashenafihagos.ah@gmail.com>
2022-03-15 21:15:54 +0300

    minor adjustments

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-_isalpha - checks for alphabetic character
+*_isalpha - checks for alphabetic character
 * @c: takes in a character
 * Return: 1 if letter, lowercase, uppercase; 0 for otherwise
 */
