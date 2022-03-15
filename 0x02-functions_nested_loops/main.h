a7bf02988b60e8c935607156390335811c9e344a
ashu hg <ashenafihagos.ah@gmail.com>
2022-03-15 21:47:58 +0300

    sd

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* main - check the code.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char x[8] = "_putchar";
+int i = 0;
+
+while (i <= 7)
+{
+_putchar(x[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
