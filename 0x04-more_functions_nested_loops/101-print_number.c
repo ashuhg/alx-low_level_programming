ca0cd55092d44fab6e921fac715d18e93b7d5c27
ashu hg <ashenafihagos.ah@gmail.com>
2022-03-18 01:27:02 +0300

    correction

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,14 +1,46 @@
-#include <stdio.h>
-#include <unistd.h>
+#include "main.h"
 
 /**
- *main - Entry point
- *Return: Always 1
+ * print_number - prints number
  *
+ * @n: integer to print to character
  */
-
-int main(void)
+void print_number(int n)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	int i;
+	int d = 1;
+	unsigned int x = n;
+	unsigned int y = n;
+	int c = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n + 1;
+		n = -n;
+		y = n;
+		x = n;
+		x += 1;
+		y += 1;
+	}
+
+	while (x != 0)
+	{
+		x = x / 10;
+		c++;
+	}
+	for (i = 1; i < c; i++)
+	{
+		d *= 10;
+	}
+	for (i = 0; i < c; i++)
+	{
+		_putchar(y / d + '0');
+		y = y % d;
+		d = d / 10;
+	}
 }
