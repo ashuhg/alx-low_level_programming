4a191412e8fc5b99d7c7a0fb230c7efe941d94f1
ashu hg <ashenafihagos.ah@gmail.com>
2022-03-21 23:22:51 +0300

    dfgh

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-* reset_to_98 - updates the value *n points to.
-* @n: the number to modify.
+* reset_to_98 - reset argument to 98
+* @n: - Pointer to int
+* Return: void
 */
-
 void reset_to_98(int *n)
 {
 *n = 98;
