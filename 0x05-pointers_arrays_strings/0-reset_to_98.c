f23e70d5442120297d3e06459fc2ca81b75cb126
ashu hg <ashenafihagos.ah@gmail.com>
2022-03-21 20:59:16 +0300

    betty code

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,11 @@
 #include "main.h"
 
-/*
- *reset_to_98-updates the value of *n points to
- *@n number to modify
- */
+/**
+* reset_to_98 - updates the value *n points to.
+* @n: the number to modify.
+*/
 
-void reset_to_98(int*n)
+void reset_to_98(int *n)
 {
-
 *n = 98;
-
-
 }	
